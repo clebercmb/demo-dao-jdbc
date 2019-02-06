17e69a2102dab8c77a674e87bc1a6164bb3dcea3
Cleber Miranda Barbosa <cleber@gmail.com>
2019-02-06 14:54:17 -0200

    FindById Implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,13 +1,21 @@
 package application;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
+import model.entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
 
+//		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+//		LocalDate.parse("22/04/1985", formatter);
 		SellerDao sellerDao = DaoFactory.createSellerDao();
+		Seller seller = sellerDao.findById(10);
+		System.out.println(seller);
 	}
 
 }
--- src/model/dao/impl/SellerDaoJDBC.java
+++ src/model/dao/impl/SellerDaoJDBC.java
@@ -1,12 +1,23 @@
 package model.dao.impl;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.util.List;
 
+import db.DB;
 import model.dao.SellerDao;
+import model.entities.Department;
 import model.entities.Seller;
 
 public class SellerDaoJDBC implements SellerDao {
 
+	private Connection conn;
+	
+	public SellerDaoJDBC (Connection conn) {
+		this.conn = conn;
+	}
+	
 	@Override
 	public void insert(Seller seller) {
 		// TODO Auto-generated method stub
@@ -27,8 +38,44 @@ public class SellerDaoJDBC implements SellerDao {
 
 	@Override
 	public Seller findById(Integer id) {
-		// TODO Auto-generated method stub
-		return null;
+	
+		PreparedStatement st = null;
+		ResultSet rs = null;
+		Seller seller = null;
+		
+		try {
+			st = conn.prepareStatement("SELECT seller.*,department.Name as DepName\n" 
+					+ "FROM seller INNER JOIN department\n" 
+					+ "ON seller.DepartmentId = department.Id\n" 
+					+ "WHERE seller.Id = ?");
+			st.setInt(1, id);
+			
+			rs = st.executeQuery();
+			
+			if ( rs.next() ) {
+				String name = rs.getString("Name");
+				String email = rs.getString("Email");
+				java.sql.Date date = rs.getDate("BirthDate");
+				Double baseSalary = rs.getDouble("BaseSalary");
+				Integer departmentId = rs.getInt("DepartmentId");
+				String depName = rs.getString("DepName");
+				
+				Department dp =  new Department(departmentId,depName);
+				
+				seller = new Seller(id, name, email, date.toLocalDate(), baseSalary, dp);
+			}
+			
+		} 
+		catch (Exception e)  
+		{
+			e.printStackTrace();
+		}
+		finally {
+			DB.closeStatement(st);
+			DB.closeResulSet(rs);
+		}
+		
+		return seller;
 	}
 
 	@Override
